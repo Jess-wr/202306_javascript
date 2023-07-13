7d0785e4ce528e8b086d329681da202bcc7e9c1a
Jess-wr <131405468+Jess-wr@users.noreply.github.com>
2023-07-13 15:07:38 -0700

    Add files via upload

---
+++ CORE - likes/script.js
@@ -0,0 +1,22 @@
+var like1 = 9
+function addLike1(){
+    like1++;
+    var likeElement = document.querySelector("#like1")
+    likeElement.innerText = like1   
+    console.log(like1)
+}
+var like2 = 12
+function addLike2(){
+    like2++;
+    var likeElement = document.querySelector("#like2")
+    likeElement.innerText = like2
+    console.log(like2)
+}
+
+var like3 = 9
+function addLike3(){
+    like3++;
+    var likeElement = document.querySelector("#like3")
+    likeElement.innerText = like3
+    console.log(like3)
+}
